9ff50194e5998b878d6c7fdffeebe91c1eeb3dd0
HariBalaji96 <harianandbalaji@gmail.com>
2023-08-03 09:40:00 +0530

    Navbar is Completed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div className=''>
-        <h1 className='text-red-600 text-3xl font-bold cursor-pointer'>NETFLIX</h1>
-        <div>
-            <button>Sign In</button>
-            <button>Sign Up</button>
-        </div>
-    </div>
-  )
-}
+    <nav className="flex bg-black z-[100] w-full absolute">
+      <h1 className="text-red-600 text-4xl font-bold cursor-pointer m-2">
+        NETFLIX
+      </h1>
+      <div className="ml-auto">
+        <button className="border-2 border-red-600 shadow-red-600 text-white px-6 py-2 rounded-md cursor-pointer m-2">
+          Sign In
+        </button>
+        <button className="bg-red-600 text-white px-6 py-2 rounded-md cursor-pointer m-2">
+          Sign Up
+        </button>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
